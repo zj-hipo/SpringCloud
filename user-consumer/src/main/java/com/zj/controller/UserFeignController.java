9326ba8834e099abd9027799f18afcfe8894d567
zj-hipo <542699293@qq.com>
2020-08-05 08:11:51 +0800

    这个版本测试了eureka、hystrix、ribbon；但是feign没有测试完成

--- user-consumer/src/main/java/com/zj/controller/UserFeignController.java
+++ user-consumer/src/main/java/com/zj/controller/UserFeignController.java
@@ -1,14 +1,15 @@
 package com.zj.controller;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.zj.client.UserFeignClient;
 import com.zj.pojo.News;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @program: springCloud1
@@ -17,14 +18,24 @@ import org.springframework.web.client.RestTemplate;
  * @create: 2020-07-15 16:09
  **/
 @Controller
-@RequestMapping("/user-feign")
+@RequestMapping("/user-consumer")
 public class UserFeignController {
     @Autowired
-    private UserFeignClient userFeignClient;
+    private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @RequestMapping("/{id}")
     @ResponseBody
     public News query(@PathVariable int id){
-        News news=userFeignClient.feignQuery(id);
+        //根据服务id获取实例
+        discoveryClient.getInstances("user-provider");
+        // 根据服务名称，获取服务实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("user-provider");
+        // 因为只有一个UserService,因此我们直接get(0)获取
+        ServiceInstance instance = instances.get(0);
+        // 获取ip和端口信息
+        String baseUrl = "http://"+instance.getHost() + ":" + instance.getPort()+"/"+id;
+        News news=restTemplate.getForObject(baseUrl,News.class);
         return news;
     }
 }
